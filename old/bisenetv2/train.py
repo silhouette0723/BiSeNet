b86352c5b429360a11d4716c8cff2a1ab0cd7db7
th <xietianhan0723@outlook.com>
2025-03-28 19:54:19 +0800

    fix train.py

--- old/bisenetv2/train.py
+++ old/bisenetv2/train.py
@@ -16,8 +16,6 @@ import torch.nn as nn
 import torch.distributed as dist
 from torch.utils.data import DataLoader
 
-from bisenetv2.bisenetv2_prune import BiSeNetV2
-
 # from bisenetv2.bisenetv2 import BiSeNetV2
 from bisenetv2.cityscapes_cv2 import get_data_loader
 from bisenetv2.evaluatev2 import eval_model
@@ -47,11 +45,15 @@ torch.backends.cudnn.deterministic = True
 #  torch.multiprocessing.set_sharing_strategy('file_system')
 
 lr_start = 5e-2
-warmup_iters = 1000
+warmup_iters = 3000
 max_iter = 150000  + warmup_iters
 ims_per_gpu = 8
 
-
+d = {}
+d["190"] = 1
+d["210"] = 2
+d["213"] = 2
+d["233"] = 3
 
 def parse_args():
     parse = argparse.ArgumentParser()
@@ -60,19 +62,22 @@ def parse_args():
     parse.add_argument('--fp16', dest='use_fp16', action='store_true',)
     parse.add_argument('--port', dest='port', type=int, default=44554,)
     parse.add_argument('--respth', dest='respth', type=str, default='./bisenetv2/res',)
+    parse.add_argument('--loadmodel_size', dest='loadmodel_size', type=int)
     return parse.parse_args()
 
 args = parse_args()
 
-
-
 def set_model():
-    net = BiSeNetV2(2)
+    varient = args.loadmodel_size
+    model_name = f"bisenetv2.bv2_{varient}MB"
+    net_module = __import__(model_name, fromlist=["bisenetv2"])
+    net = net_module.BiSeNetV2(2)
     if args.use_sync_bn: net = set_syncbn(net)
     net.cuda()
     net.train()
+    num = d[f'{varient}']
     criteria_pre = OhemCELoss(0.7)
-    criteria_aux = [OhemCELoss(0.7) for _ in range(4)]
+    criteria_aux = [OhemCELoss(0.7) for _ in range(num)]
     return net, criteria_pre, criteria_aux
 
 def set_syncbn(net):
@@ -111,7 +116,8 @@ def set_model_dist(net):
         net = nn.parallel.DistributedDataParallel(
             net,
             device_ids=[local_rank, ],
-            output_device=local_rank)
+            output_device=local_rank,
+            find_unused_parameters=True)
     return net
 
 
@@ -119,7 +125,8 @@ def set_meters():
     time_meter = TimeMeter(max_iter)
     loss_meter = AvgMeter('loss')
     loss_pre_meter = AvgMeter('loss_prem')
-    loss_aux_meters = [AvgMeter('loss_aux{}'.format(i)) for i in range(4)]
+    num = d[f"{args.loadmodel_size}"]
+    loss_aux_meters = [AvgMeter('loss_aux{}'.format(i)) for i in range(num)]
     return time_meter, loss_meter, loss_pre_meter, loss_aux_meters
 
 
@@ -137,8 +144,8 @@ def train(epochs=10):  # 允许传入 epochs 参数，默认为 100 轮
     logger = logging.getLogger()
     is_dist = dist.is_initialized()
 
-    dataset = Hair_Dt("val")
-    dl = DataLoader(dataset, batch_size=16, shuffle=True)
+    dataset = Hair_Dt("train")
+    dl = DataLoader(dataset, batch_size=64, shuffle=True, num_workers=4)
 
     net, criteria_pre, criteria_aux = set_model()
     optim = set_optimizer(net)
@@ -166,11 +173,6 @@ def train(epochs=10):  # 允许传入 epochs 参数，默认为 100 轮
             optim.zero_grad()
             logits, *logits_aux = net(im)
             loss_pre = criteria_pre(logits, lb)
-            # print(f"22222222:{logits_aux[0].shape}")
-            # print(f"22222222:{logits_aux[1].shape}")
-            # print(f"22222222:{logits_aux[2].shape}")
-            # print(f"22222222:{logits_aux[3].shape}")
-            # print(f"22222222:{len(logits_aux)}")
             loss_aux = [crit(lgt, lb) for crit, lgt in zip(criteria_aux, logits_aux)]
             loss = loss_pre + sum(loss_aux)
 
@@ -194,17 +196,13 @@ def train(epochs=10):  # 允许传入 epochs 参数，默认为 100 轮
                 print_log_msg(it, max_iter, lr, time_meter, loss_meter, loss_pre_meter, loss_aux_meters)
 
         # 每个 epoch 结束后保存模型
-        # if (epoch + 1) % 10 == 0:  # 例如，每 10 个 epoch 保存一次
-        if True:
-            save_pth = osp.join(args.respth, f'model_epoch_{epoch+1}.pth')
+        if (epoch + 1) % 25 == 0:  # 例如，每 10 个 epoch 保存一次
+        # if True:
+            save_pth = osp.join(args.respth, f'model_{args.loadmodel_size}MB_epoch_{epoch+1}.pth')
             if dist.get_rank() == 0:
                 torch.save(net.module.state_dict(), save_pth)
             logger.info(f'Model saved at epoch {epoch+1}')
 
-    # 训练结束后保存最终模型
-    save_pth = osp.join(args.respth, 'model_final.pth')
-    if dist.get_rank() == 0:
-        torch.save(net.module.state_dict(), save_pth)
     logger.info('\nFinal model saved')
 
     logger.info('\nEvaluating the final model...')
